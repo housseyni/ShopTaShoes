5116e1e73ce8fb946edc00ba8fbaf243a4abd5b3
housseyni <alhousseyni.ndiaye@outlook.fr>
2023-11-21 12:09:38 +0100

    confirmation livewire

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -2,12 +2,53 @@
 
 namespace App\Livewire;
 
+use App\Models\Shoe;
+
+use Illuminate\Support\Collection;
+
 use Livewire\Component;
 
 class Counter extends Component
 {
-
+    public string $nom;
+    public string $marque;
     public string $username;
+    public int $count;
+    public Collection $shoes;
+
+
+
+    public function mount()
+    {
+        $this->username = "Houss";
+        $this->count = "0";
+        $this->shoes = Shoe::all();
+    }
+
+
+    public function increment()
+    {
+        $this->count++;
+    }
+
+    public function decrement()
+    {
+        $this->count--;
+    }
+
+    function add()
+    {
+        $s = new Shoe();
+        $s->user_id = 1;
+        $s->nom = $this->nom;
+        $s->marque = $this->marque;
+        $s->taille = 43;
+        $s->couleur = "noir";
+        $s->price = "200";
+        $s->save();
+        $this->shoes = Shoe::all()->sortByDesc('id');
+    }
+
 
     public function render()
     {
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,6 +1,51 @@
 <div>
+ {{-- <div class="flex items-center justify-center space-x-4 p-4 bg-gray-100">
     {{-- The Master doesn't talk, he acts. --}}
 
-    <input wire:model.live="username" type="text">
-    {{$username}}
+    {{-- <input wire:model.live="username" type="text" class="border border-gray-300 p-2 rounded">
+    <p class="text-gray-700">{{$username}}</p>
+    
+
+    <button wire:click="increment" class="bg-green-500 text-white px-4 py-2 rounded">+</button>
+
+    <p class="text-gray-700">{{$count}}</p>
+
+    <button wire:click="decrement" class="bg-red-500 text-white px-4 py-2 rounded">-</button>
+
+
+
+    </div> --}} 
+
+    <div>
+        <x-input wire:model="nom" type = "text" />
+        <x-input wire:model="marque" type ="text" />
+        <x-button wire:click="add" >Ajouter</x-button>
+        <table class="min-w-full border border-gray-300">
+            <thead>
+                <tr class="bg-gray-100">
+                    
+                    <x-th >ID</x-th>
+                    <x-th >Nom</x-th>
+                    <x-th >Marque</x-th>
+                    <x-th >Taille</x-th>
+                    <x-th >Couleur</x-th>
+                    <x-th >Prix</x-th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($shoes as $shoe)
+                    <tr>
+                        
+                        <x-td >{{ $shoe->id }}</x-td>
+                        <x-td >{{ $shoe->nom }}</x-td>
+                        <x-td >{{ $shoe->marque }}</x-td>
+                        <x-td >{{ $shoe->taille }}</x-td>
+                        <x-td >{{ $shoe->couleur }}</x-td>
+                        <x-td >{{ $shoe->price }}</x-td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    
+    </div>
 </div>
